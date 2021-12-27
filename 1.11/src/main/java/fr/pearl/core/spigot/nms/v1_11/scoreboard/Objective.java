586afb9fbe4096430801755d6721eb3186a7155f
Ibramsou <kasobanboui@gmail.com>
2021-12-28 00:05:31 +0100

    rename nms packages

---
+++ 1.11/src/main/java/fr/pearl/core/spigot/nms/v1_11/scoreboard/Objective.java
@@ -0,0 +1,34 @@
+package fr.pearl.core.spigot.nms.v1_11.scoreboard;
+
+import fr.pearl.api.spigot.nms.scoreboard.NmsObjective;
+import net.minecraft.server.v1_11_R1.*;
+
+public class Objective extends ScoreboardObjective implements NmsObjective<ScoreboardObjective> {
+
+    private String displayName;
+
+    public Objective(String name, ScoreboardServer server, IScoreboardCriteria criteria) {
+        super(server, name, criteria);
+        this.displayName = name;
+    }
+
+    @Override
+    public String getCurrentDisplayName() {
+        return getDisplayName();
+    }
+
+    @Override
+    public String getDisplayName() {
+        return this.displayName;
+    }
+
+    @Override
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public ScoreboardObjective getServerObjective() {
+        return this;
+    }
+}
---
+++ 1.13/src/main/java/fr/pearl/core/spigot/nms/v1_13/packet/outbound/ServerPlayerInfo.java
@@ -0,0 +1,87 @@
+package fr.pearl.core.spigot.nms.v1_13.packet.outbound;
+
+import fr.pearl.api.common.util.Reflection;
+import fr.pearl.api.spigot.packet.PacketServer;
+import fr.pearl.api.spigot.packet.registry.outbound.NmsPacketServerPlayerInfo;
+import fr.pearl.api.spigot.packet.registry.enums.PlayerInfoType;
+import net.minecraft.server.v1_13_R2.EntityPlayer;
+import net.minecraft.server.v1_13_R2.PacketPlayOutPlayerInfo;
+import org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.EnumMap;
+import java.util.function.Supplier;
+
+public class ServerPlayerInfo implements NmsPacketServerPlayerInfo {
+
+    private static final EnumMap<PlayerInfoType, PacketPlayOutPlayerInfo.EnumPlayerInfoAction> toServerInfoMap = new EnumMap<>(PlayerInfoType.class);
+    private static final EnumMap<PacketPlayOutPlayerInfo.EnumPlayerInfoAction, PlayerInfoType> toNmsInfoMap = new EnumMap<>(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.class);
+    private static final Field actionField = Reflection.access(PacketPlayOutPlayerInfo.class, "a");
+
+    static {
+        PlayerInfoType[] infoTypes = PlayerInfoType.values();
+        PacketPlayOutPlayerInfo.EnumPlayerInfoAction[] infoActions = PacketPlayOutPlayerInfo.EnumPlayerInfoAction.values();
+        for (int i = 0; i < infoActions.length; i++) {
+            PlayerInfoType infoType = infoTypes[i];
+            PacketPlayOutPlayerInfo.EnumPlayerInfoAction infoAction = infoActions[i];
+            toServerInfoMap.put(infoType, infoAction);
+            toNmsInfoMap.put(infoAction, infoType);
+        }
+    }
+
+    private PacketPlayOutPlayerInfo.EnumPlayerInfoAction infoAction = null;
+    private EntityPlayer[] entityPlayers = null;
+
+    @Override
+    public void setPlayerInfoType(PlayerInfoType infoType) {
+        this.infoAction = toServerInfoMap.get(infoType);
+    }
+
+    @Override
+    public void setPlayers(Player... players) {
+        EntityPlayer[] entityPlayers = new EntityPlayer[players.length];
+        for (int i = 0; i < players.length; i++) {
+            entityPlayers[i] = ((CraftPlayer) players[0]).getHandle();
+        }
+
+        this.entityPlayers = entityPlayers;
+    }
+
+    @Override
+    public void setPlayers(Collection<? extends Player> players) {
+        EntityPlayer[] entityPlayers = new EntityPlayer[players.size()];
+        int index = 0;
+        for (Player player : players) {
+            entityPlayers[index] = ((CraftPlayer) player).getHandle();
+            index++;
+        }
+
+        this.entityPlayers = entityPlayers;
+    }
+
+    @Override
+    public PlayerInfoType getPlayerInfoType() {
+        return toNmsInfoMap.get(this.infoAction);
+    }
+
+    @Override
+    public Object getPacket() {
+        return new PacketPlayOutPlayerInfo(this.infoAction, this.entityPlayers);
+    }
+
+    @Override
+    public Class<?> packetClass() {
+        return PacketPlayOutPlayerInfo.class;
+    }
+
+    @Override
+    public void setValues(Object packet) {
+        this.infoAction = Reflection.get(actionField, packet);
+    }
+
+    public static Supplier<PacketServer> getSupplier() {
+        return ServerPlayerInfo::new;
+    }
+}
