b6701172e1620098734b7a2b4d274720ea5bf3be
Ibramsou <kasobanboui@gmail.com>
2021-12-28 00:06:36 +0100

    Remove test

--- spigot/src/main/java/fr/pearl/core/spigot/listener/ConnectionListener.java
+++ spigot/src/main/java/fr/pearl/core/spigot/listener/ConnectionListener.java
@@ -1,11 +1,7 @@
 package fr.pearl.core.spigot.listener;
 
 import fr.pearl.api.spigot.PearlSpigot;
-import fr.pearl.api.spigot.nms.scoreboard.NmsObjective;
-import fr.pearl.api.spigot.packet.registry.ServerRegistry;
-import fr.pearl.api.spigot.packet.registry.outbound.NmsPacketServerScoreboardObjective;
 import fr.pearl.core.spigot.packet.PacketManager;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,15 +14,6 @@ public class ConnectionListener implements Listener {
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         ((PacketManager) PearlSpigot.getInstance().getPacketManager()).addPlayer(player, false);
-
-        NmsObjective<?> objective = PearlSpigot.getInstance().getNmsManager().getNms().createScoreboard().createObjective("Bramsou", "dummy");
-
-        Bukkit.getScheduler().runTaskLater(PearlSpigot.getInstance(), () -> {
-            NmsPacketServerScoreboardObjective scoreboardObjective = ServerRegistry.SCOREBOARD_OBJECTIVE.getPacket();
-            scoreboardObjective.setObjective(objective);
-            scoreboardObjective.setType(0);
-            scoreboardObjective.send(player);
-        }, 1L);
     }
 
     @EventHandler
